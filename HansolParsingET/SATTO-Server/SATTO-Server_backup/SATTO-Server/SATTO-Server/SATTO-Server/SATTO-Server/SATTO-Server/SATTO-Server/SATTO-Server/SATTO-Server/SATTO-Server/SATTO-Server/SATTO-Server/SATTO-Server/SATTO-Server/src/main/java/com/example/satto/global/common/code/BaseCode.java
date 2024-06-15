91528f7305857e2aa6b7551a3a3b69de467250e6
Lee Hansol <79439483+EstherHansol@users.noreply.github.com>
2024-06-15 23:14:55 +0900

    Delete HansolParsingET directory

--- HansolParsingET/SATTO-Server/SATTO-Server_backup/SATTO-Server/SATTO-Server/SATTO-Server/SATTO-Server/SATTO-Server/SATTO-Server/SATTO-Server/SATTO-Server/SATTO-Server/SATTO-Server/SATTO-Server/SATTO-Server/SATTO-Server/src/main/java/com/example/satto/global/common/code/BaseCode.java
+++
@@ -1,9 +0,0 @@
-package com.example.satto.global.common.code;
-
-public interface BaseCode {
-
-    public ReasonDTO getReason();
-
-    public ReasonDTO getReasonHttpStatus();
-
-}
--- HansolParsingET/src/main/java/com/example/satto/global/common/code/BaseErrorCode.java
+++
@@ -1,9 +0,0 @@
-package com.example.satto.global.common.code;
-
-public interface BaseErrorCode {
-
-    public ErrorReasonDTO getReason();
-
-    public ErrorReasonDTO getReasonHttpStatus();
-
-}
